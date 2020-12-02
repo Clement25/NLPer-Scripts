58cacf53236e8e499272204110101e923c8590fc
Clement25 <935974082@qq.com>
2020-12-02 20:19:10 +0800

    add remove randomness template and update README.md

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Useful-Scripts for NLPers
 Collection of scripts for obtaining NLP data and depolying environment
 
+## Introduction
+| Folder | Introduction |
+| -- | -- |
+| config | scripts of configurations for machine learning tools in linux, like tmux, etc. |
+| downloads | scripts to get common pretrained vectors and datasets |
+| pytorch_solutions | code segments to solve common issues in training process |
+
 ## Usage
 
 For any scripts, use following command to execute
---
+++ pytorch_solutions/remove_randomness.py
@@ -0,0 +1,20 @@
+import torch
+import numpy as np
+import random
+
+seed=0
+
+random.seed(seed)
+np.random.seed(seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(seed)
+
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+
+# Remove randomness (may be slower on Tesla GPUs) 
+# https://pytorch.org/docs/stable/notes/randomness.html
+if seed == 0:
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
